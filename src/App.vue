cb2ac3e3d521619b909d283b926dfb6b9c7392e6
PendingUsername <cook.joshual@yahoo.com>
2024-01-03 20:58:00 -0500

    Added income and expenses.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <Header />
     <div class="container">
-        <Balance />
-        <Expenses />
+        <Balance :total="total" />
+        <Expenses :income="income" :expenses="expenses" />
         <Transactions :transactions="transactions"  />
         <add/>
     </div>
@@ -15,7 +15,7 @@ import Expenses from './components/Expenses.vue';
 import Transactions from './components/Transactions.vue';
 import Add from './components/Add.vue';
 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const transactions = ref([
           { id: 1, name: 'Cash', amount: -400 },
@@ -23,4 +23,31 @@ const transactions = ref([
           { id: 3, name: 'Computer', amount: -800 },
           { id: 4, name: 'Paycheck', amount: 800 },
 ]);
+// Total 
+const total = computed(() => {
+    return transactions.value
+        .filter(transaction => transaction.amount > 0)
+        .reduce((acc, transaction) => {
+            return acc + transaction.amount;
+        }, 0)
+        .toFixed(2);
+});
+
+
+// Income
+const income = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+        return acc + transaction.amount;
+    }, 0);
+});
+
+// Expenses
+const expenses = computed(() => {
+    return transactions.value
+        .filter(transaction => transaction.amount < 0)
+        .reduce((acc, transaction) => {
+            return acc + transaction.amount;
+        }, 0)
+        .toFixed(2);
+});
 </script>
